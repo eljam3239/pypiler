96b99924530f78e89552a005bf7b2ebf8da7c19b
eljam3239 <elijames2301@gmail.com>
2024-05-11 15:52:43 -0400

    Write Lexer boilerplate

---
+++ lex.py
@@ -0,0 +1,22 @@
+class Lexer:
+    def __init__(self, source):
+        pass
+
+    def nexChar(self):
+        pass
+
+    def peek(self):
+        pass
+
+    #invalid token, exit
+    def abort(self, message):
+        pass
+
+    def skipWhiteSpace(self):
+        pass
+
+    def skipComment(self):
+        pass
+
+    def getToken(self):
+        pass
