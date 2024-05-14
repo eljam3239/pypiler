c3e8e0d18a8c27347ce211d3b982926480e2c939
Eli <elijames2301@gmail.com>
2024-05-14 14:56:46 -0400

    Write match, init

--- parse.py
+++ parse.py
@@ -3,19 +3,26 @@ from lex import *
 
 class Parser:
     def __init__(self, lexer):
-        pass
+        self.lexer = lexer
+        self.curToken = None
+        self.peekToken = None
+        self.nextToken()
+        self.nextToken()
     
     def checkToken(self, kind):
-        pass
+        return kind == self.curToken.kind
 
     def checkPeek(self, kind):
-        pass
+        return kind == self.peekToken.kind
 
     def match(self, kind):
-        pass
+        if not self.checkToken(kind):
+            self.abort("Expected " + kind.name + ", got " + self.curToken.kind.name)
+        self.nextToken()
 
     def nextToken(self):
-        pass
+        self.curToken = self.peekToken
+        self.peekToken = self.lexer.getToken()
 
     def abort(self, message):
         sys.exit("Error. " + message)
